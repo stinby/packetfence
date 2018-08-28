577e6a324a26267c4d717b2951cf0712db2d28bd
Durand Fabrice <oeufdure@gmail.com>
2018-08-28 09:40:27 -0400

    Added vip address in the dhcp server list.

--- lib/pfconfig/namespaces/resource/network_config.pm
+++ lib/pfconfig/namespaces/resource/network_config.pm
@@ -53,6 +53,7 @@ sub build {
                 if ($net_addr->contains($ip)) {
                     $ConfigNetwork{$network}{'cluster_ips'} = join(',', map { $_->{"interface ".$interface{'int'}}->{ip}} @{$self->{cluster_resource}->{_servers}});
                     $ConfigNetwork{$network}{'dns_vip'} = $self->{cluster_resource}->{cfg}->{CLUSTER}->{'interface '. $interface{'int'}}->{ip} || $interface{'ip'};
+                    $ConfigNetwork{$network}{'vip'} = $self->{cluster_resource}->{cfg}->{CLUSTER}->{'interface '. $interface{'int'}}->{ip} || $interface{'ip'};
                     $ConfigNetwork{$network}{'interface'} = \%interface;
                 }
             } else {
@@ -60,6 +61,7 @@ sub build {
                 if ($net_addr->contains($ip)) {
                     $ConfigNetwork{$network}{'cluster_ips'} = join(',', map { $_->{"interface ".$interface{'int'}}->{ip}} @{$self->{cluster_resource}->{_servers}});
                     $ConfigNetwork{$network}{'dns_vip'} = $self->{cluster_resource}->{cfg}->{CLUSTER}->{'interface '. $interface{'int'}}->{ip} || $interface{'ip'};
+                    $ConfigNetwork{$network}{'vip'} = $self->{cluster_resource}->{cfg}->{CLUSTER}->{'interface '. $interface{'int'}}->{ip} || $interface{'ip'};
                     $ConfigNetwork{$network}{'interface'} = \%interface;
                 }
             }
